47c991a50ceb6dce21e9b23531b8806b6b7ccf23
Grzegorz Szermiński <szerman@poczta.fm>
2018-04-26 10:22:15 +0200

    Exercise finished

--- src/App.js
+++ src/App.js
@@ -1,39 +1,33 @@
 import React, { Component } from 'react';
-import { Router, Route, IndexRoute, Link, hashHistory } from 'react-router';
+import { Router, Route, IndexLink, IndexRoute, Link,  hashHistory } from 'react-router';
 
 class App extends Component {
-	render() {
-		return (
-			
-<Router history={hashHistory}>
-    <Route path='/' component={Navigation}>
-        <IndexRoute component={Home} />
-        <Route path='/contact' component={Contact} />
-    </Route>
-</Router>
-		)
-	}
+  render() {
+    return (
+      <Router history ={hashHistory}>
+        <Route path='/' component={Navigation}>
+          <IndexRoute component={Home} />
+          <Route path='/contact' component={Contact} />
+          <Route path='/hello/:name' component={Hello} />
+          <Route path='*' component={PageNotFound} />
+        </Route>
+      </Router>
+    )
+  }
 }
-const Home = () => <h1>Hej, tu Home component</h1>
-const Contact = () => <h1>A tu Contact component</h1>
-export default App
 
+const Home = () => <h1>Hej, tu Home component</h1>
+const Contact = () => <h1>A tu Contact Cmonent</h1>
 const Navigation = props => (
     <div>
-       <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/contact">Contact</Link></li>
-        </ul>
-        {props.children}
+      <ul>
+        <li><IndexLink to="/" activeStyle={{color: '#48abee'}} onlyActiveOnIndex>Home</IndexLink></li>
+        <li><Link to="/contact" activeStyle={{color: '#48abee'}} onlyActiveOnIndex>Contact</Link></li>
+      </ul>
+      {props.children}
     </div>
 );
-
-
 const PageNotFound = () => <h1>404 Not Found</h1>;
-<Router history={hashHistory}>
-        <Route path='/' component={Navigation}>
-                <IndexRoute component={Home} />
-                <Route path='/contact' component={Contact} />
-        <Route path='*' component={PageNotFound} />
-        </Route>
-</Router>
+const Hello = props => <h1>Witaj, {props.params.name}</h1>
+
+export default App;
